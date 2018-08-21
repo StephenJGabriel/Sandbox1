79cc61ad3b61665343d6809f303183c7f58bfe97
popey <$tr8Arr0w5>
2018-08-21 16:03:11 -0400

    added 3 variables to class arrays - SJG

--- src/arraysandbox/Arrays.java
+++ src/arraysandbox/Arrays.java
@@ -1,10 +1,17 @@
 package arraysandbox;
 
+import java.util.Random;
+
 public class Arrays {
 
 	public static void main(String[] args) {
 		/* variables */
 		int[] dfa = new int[10]; 
+		int cntOfEntries = 10;
+        int sub = 0;  // used as subscript for the array
+        long key; // the element we'ren looking for
+        Random random = new Random(100);
+
 		
 	}
 
