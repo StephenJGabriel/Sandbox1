8bec38759c515cdd9bb0e7130067148a9f06f5c1
popey <$tr8Arr0w5>
2018-08-21 20:42:14 -0400

    added 2 loops

--- src/arraysandbox/Arrays.java
+++ src/arraysandbox/Arrays.java
@@ -8,10 +8,26 @@ public class Arrays {
 		/* variables */
 		int[] dfa = new int[10]; 
 		int cntOfEntries = 10;
-		int sub = 0;  // used as subscript for the array
 		long key; // the element we'ren looking for
 		Random random = new Random(100);
 		// editing with git
+		
+		// insert data
+		for(int i = 0; i< cntOfEntries; i++) {
+			dfa[i] = (int)(random.nextInt(100));
+		} // end insert
+		
+		
+        System.out.println("simple for loop");
+        for(int i=0; i < cntOfEntries; i++) {
+        	System.out.println(dfa[i]);
+        }
+                
+        System.out.println("ForEach loop");
+        for(int a:dfa) {
+        	System.out.print(a + ",");
+        }
+        
 
 		
 	}
